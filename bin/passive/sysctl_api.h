5d9854237d9f417aa6cc86a1d04433b9a668ce4c
Adrian Chadd <adrian@freebsd.org>
2014-04-16 18:53:44 +0000

    * Add an unused flags header. * comments.

--- bin/passive/sysctl_api.h
+++ bin/passive/sysctl_api.h
@@ -10,15 +10,22 @@ struct sysctl_req_hdr {
 	uint32_t	sysctl_str_len;
 	uint32_t	sysctl_dst_len;		/* result (new) */
 	uint32_t	sysctl_src_len;		/* request (old) */
+
+	/* sysctl string follows, non-NUL terminated */
+
+	/* srcbuf follows, if srclen != 0 */
 };
 
 struct sysctl_resp_hdr {
 	uint32_t	sysctl_resp_len;
 	uint32_t	sysctl_resp_type;
+	uint32_t	sysctl_resp_flags;
 
 	/* This is the sysctl specific stuff */
 	uint32_t	sysctl_dst_len;		/* response buffer length */
 	uint32_t	sysctl_dst_errno;	/* sysctl errno value */
+
+	/* Response follows, if sysctl_dst_len != 0 */
 };
 
 extern	void * passive_sysctl_listener(void *arg);
