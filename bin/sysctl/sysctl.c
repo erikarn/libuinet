f52004a421ea9372d94a47b8c1dead31de4bbbd4
Adrian Chadd <adrian@freebsd.org>
2014-04-16 18:54:09 +0000

    Handle the response from the server.

--- bin/sysctl/sysctl.c
+++ bin/sysctl/sysctl.c
@@ -23,29 +23,39 @@ main(int argc, char *argv[])
 	int r;
 	struct sysctl_req_hdr req_hdr;
 	struct sysctl_resp_hdr *resp_hdr;
-	char *req_buf, *resp_buf;
+	char *req_buf, *read_buf;
 	char *req_str;
 	size_t reqbuf_len, respbuf_len;
+	size_t readbuf_len;
+	size_t read_ofs = 0;
 
 	/* Fake up a request structure for now */
 	req_str = strdup("net.inet.tcp.stats");
 	reqbuf_len = 0;
 	respbuf_len = 1048576;
 	req_buf = NULL;
-	resp_buf = NULL;
+	read_buf = NULL;
 
-	/* Ok, allocate req/resp buffers as required */
+	/* Ok, allocate request buffer */
 	if (reqbuf_len > 0) {
 		req_buf = calloc(1, reqbuf_len);
 		if (req_buf == NULL)
 			err(1, "calloc");
 	}
 
-	if (respbuf_len > 0) {
-		resp_buf = calloc(1, respbuf_len);
-		if (resp_buf == NULL)
-			err(1, "calloc");
-	}
+	/*
+	 * Calculate the readbuf_len.  It's the combination
+	 * of the header size and the response payload.
+	 */
+	readbuf_len = respbuf_len + sizeof(struct sysctl_resp_hdr);
+
+	/*
+	 * Allocate the response buffer.  This includes the
+	 * response header and response payload if required.
+	 */
+	read_buf = calloc(1, readbuf_len);
+	if (read_buf == NULL)
+		err(1, "calloc");
 
 	/* Connect to the destination socket */
 	bzero(&sun, sizeof(sun));
@@ -88,6 +98,62 @@ main(int argc, char *argv[])
 	}
 
 	/* Read response */
+	while (read_ofs < readbuf_len) {
+		/* Don't try to read if we have a full buffer */
+		if (readbuf_len - read_ofs <= 0)
+			break;
+		r = read(s, read_buf + read_ofs, readbuf_len - read_ofs);
+		if (r < 0) {
+			err(1, "read");
+		} if (r == 0) {
+			fprintf(stderr, "%s: read early EOF\n", __func__);
+			break;
+		}
+
+		read_ofs += r;
+
+		/* if we don't have enough data for the header, continue */
+		if (read_ofs < sizeof(struct sysctl_resp_hdr)) {
+			continue;
+		}
+
+		/* Grab the response header */
+		resp_hdr = (struct sysctl_resp_hdr *) read_buf;
+
+		/*
+		 * Is the response length greater than respbuf_len?
+		 * Then the response is too large.  Naughty server.
+		 */
+		if (le32toh(resp_hdr->sysctl_resp_len) >
+		    respbuf_len + sizeof(struct sysctl_resp_hdr)) {
+			fprintf(stderr, "%s: resp_len (%d) is too long!\n",
+			    __func__,
+			    le32toh(resp_hdr->sysctl_resp_len));
+			break;
+		}
+
+		/* Do we have enough data to match the response length? */
+		if (read_ofs < le32toh(resp_hdr->sysctl_resp_len))
+			continue;
+
+		/* We have enough data - woo! More sanity checks! */
+
+		/*
+		 * Does the response buffer length exceed what we allocated?
+		 * again, too big a response; bad coder.
+		 */
+
+		if (le32toh(resp_hdr->sysctl_dst_len) > respbuf_len) {
+			fprintf(stderr, "%s: dst_len (%d) is too long!\n",
+			    __func__,
+			    le32toh(resp_hdr->sysctl_dst_len));
+			break;
+		}
+		printf("%s: received response: errno=%d, %d bytes\n",
+		    __func__,
+		    le32toh(resp_hdr->sysctl_dst_errno),
+		    le32toh(resp_hdr->sysctl_dst_len));
+	}
 
 	/* Done */
 	close(s);
